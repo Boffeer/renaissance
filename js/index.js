6d193e2d19c1475f489bb779b0aef80bfd29c225
Boffeer <41575886+Boffeer@users.noreply.github.com>
2021-11-26 00:27:40 +0300

    faq js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 let lazyLoadInstance = new LazyLoad();
 
-let swiper = new Swiper('.steps-slider', {
+let stepsSlider = new Swiper('.steps-slider', {
 	navigation: {
 		nextEl: '.steps-slider__button-next',
 		prevEl: '.steps-slider__button-prev',
@@ -9,4 +9,40 @@ let swiper = new Swiper('.steps-slider', {
 		el: '.steps-slider__pagination'
 	},
 	spaceBetween: 50,
+});
+
+
+// if (faqCards !== null) {
+// 	faqCards.forEach(card => {
+// 		const question = card.querySelector('.faq-cards-card-question');
+// 		question.addEventListener('click', (event) => {
+// 			if (question.parentNode.classList.value.includes(faqOpenedClass)) {
+// 				question.parentNode.classList.remove(faqOpenedClass);
+// 			} else {
+// 				question.parentNode.classList.add(faqOpenedClass);
+// 			}
+// 			const answer = event.target.nextElementSibling;
+// 		})
+// 	})
+// }
+
+
+const dropdowns = [...document.querySelectorAll('.faq-cards-card-question')];
+const dropdownOpenedClass = 'opened';
+dropdowns.forEach((dropdown, index) => {
+  dropdown.addEventListener("click", function () {
+    const currentAnswer = this.parentElement.querySelector('.faq-cards-card-answer')
+    let currentAnswerHeight = currentAnswer.clientHeight;
+
+    if (this.parentElement.classList.value.includes(dropdownOpenedClass)) {
+      currentAnswer.style.marginTop = '0';
+      this.parentElement.classList.remove(dropdownOpenedClass);
+    } else {
+      currentAnswer.style.marginTop = `-${currentAnswerHeight}px`;
+      this.parentElement.classList.add(dropdownOpenedClass);
+    }
+  });
+  if (index > 0) {
+    dropdown.click();
+  }
 });
