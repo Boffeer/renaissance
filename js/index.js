336cb50c992e2954904f1ebdbf6549fdca09fd37
Boffeer <41575886+Boffeer@users.noreply.github.com>
2021-11-25 16:45:21 +0300

    add steps frontend add class for arrowed button

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 let lazyLoadInstance = new LazyLoad();
+
+let swiper = new Swiper('.steps-slider', {
+	navigation: {
+		nextEl: '.steps-slider__button-next',
+		prevEl: '.steps-slider__button-prev',
+	},
+	pagination: {
+		el: '.steps-slider__pagination'
+	},
+	spaceBetween: 50,
+});
