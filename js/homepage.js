c86d86c43891546a7fd680e0866ecca5c03a2801
Boffeer <41575886+Boffeer@users.noreply.github.com>
2021-11-27 20:49:42 +0300

    remove unused js

--- js/homepage.js
+++ js/homepage.js
@@ -33,138 +33,11 @@ servicesCards.forEach(card => {
 
 
 
-  /**
-   * Sticky Header
-   */
-//   let isWindowScrolled = $(this).scrollTop() > 100;
-//   let isStickyHeaderAllowed = window.innerWidth >= 768;
-//   function stickyHeader() {
-//     if ($(this).scrollTop() > 100 && window.innerWidth >= 768) {
-//       $(".header-menu").addClass("header-menu--scrolled");
-//     } else {
-//       $(".header-menu").removeClass("header-menu--scrolled");
-//     }
-//   }
-//   $(window).on("scroll", function () {
-//     stickyHeader();
-//   });
-//   stickyHeader();
-
-//   // MASK
-//   $("input[name=tel").inputmask("+7 (999) 999-99-99");
-
-//   /**
-//    * DROPDOWN
-//    */
-//   $(".faq-list-card-top").each(function () {
-//     $(this).on("click", function () {
-//       $(this).toggleClass("faq-list-card-top--opened");
-//       $(this).parent().find(".faq-list-card-bottom").slideToggle();
-//     });
-//   });
-
-//   /**
-//    * TABS
-//    */
-//   $(".cases-showcase-tabbar__button").each(function () {
-//     $(this).on("click", function () {
-//       $(".cases-showcase-tabbar__button").removeClass(
-//         "cases-showcase-tabbar__button--current"
-//       );
-//       $(this).addClass("cases-showcase-tabbar__button--current");
-//       var target = $(this).attr("data-target");
-
-//       $(".cases-showcase-tabs-tab").hide();
-//       $('.cases-showcase-tabs-tab[data-target="' + target + '"]').show();
-
-//       $("html, body").animate(
-//         { scrollTop: $(".cases").offset().top - 40 },
-//         800
-//       );
-//     });
-//   });
-
-//   /**
-//    * POPUPS
-//    */
-//   modality({
-//     pop: ".form--header-menu-callback",
-//     clickTrigger: ".header-menu__callback",
-//     popCloserType: "inner",
-//   });
-//   modality({
-//     pop: ".pop-thanks",
-//     clickTrigger: ".pop-thanks",
-//     popCloserType: "inner",
-//   });
-
-//   /**
-//    * AJAX SEND
-//    */
-//   $(".form").each(function () {
-//     $(this).on("submit", function (event) {
-//       event.preventDefault();
-//       let userName = $(this).find('input[name="user-name"]');
-//       let userTel = $(this).find('input[name="user-tel"]');
-//       // console.log('аякс пошел', userName.val(), userTel.val(), $(this));
-//       $.ajax({
-//         url: "send.php",
-//         type: "POST",
-//         dataType: "json",
-//         data: {
-//           user_name: userName.val(),
-//           user_tel: userTel.val(),
-//         },
-//         success: function (data) {},
-//       });
-//       $(this)[0].reset();
-//       // console.log('отправляем')
-//       ym(82308838, "reachGoal", "form-submit");
-//       closePop(
-//         document.querySelector(".form--header-menu-callback-wrapper"),
-//         document.querySelector(".form--header-menu-callback")
-//       );
-//       showPop(
-//         document.querySelector(".pop-thanks-wrapper"),
-//         document.querySelector(".pop-thanks")
-//       );
-//       setTimeout(function () {
-//         closePop(
-//           document.querySelector(".pop-thanks-wrapper"),
-//           document.querySelector(".pop-thanks")
-//         );
-//       }, 5000);
-//       return false;
-//     });
-//   });
-
-// let servicesMasonry = new Masonry( '.services-list', {
-// 	itemSelector: '.services-list-card',
-// 	columnWidth: '.services-list-card',
-// 	gutter: '.services-list-card--gutter',
-// });
-
-/*
-	{
-		brakpoint: number
-		swiper: swiper element class
-		wrapper: swiper-wrapper element
-		slide:
-		masonry: contianer
-		gutter: class
-	}
-*/
-
 let articlesMasonry;
 let articlesSlider;
 let servicesMasonry;
 let servicesSlider;
 
-// let articlesMasonry = new Masonry('.articles-list', {
-// 	itemSelector: '.articles-list-article',
-// 	columnWidth: '.articles-list-article',
-// 	gutter: '.articles-list-article--gutter',
-// })
 const resizeConstructors = [
 	{
 		breakpoint: 992,
@@ -263,14 +136,9 @@ resizeConstructors.forEach(constructor => {
 	useCorrectServicesLayout(constructor)
 })
 window.addEventListener('resize', () => {
-	// if (timeOut != undefined) {
-	// 	clearTimeout(timeOut)
-	// }
-	// var timeOut = setTimeout(() => {
-		resizeConstructors.forEach(constructor => {
-			setTimeout(() => {
-				useCorrectServicesLayout(constructor)
-			}, 2000);
-		})
-	// }, 1000)
+	resizeConstructors.forEach(constructor => {
+		setTimeout(() => {
+			useCorrectServicesLayout(constructor)
+		}, 2000);
+	})
 });
